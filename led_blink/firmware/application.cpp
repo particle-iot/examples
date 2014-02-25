4966a6cd6f629e06cbc814f45ce08a7f42da4e7c
Joe Goggins <joe.goggins@gmail.com>
2014-02-25 13:51:24 -0600

    [led_blink] change to include D7 in blinking so wiring isn't strictly necessarly

--- led_blink/firmware/application.cpp
+++ led_blink/firmware/application.cpp
@@ -1,18 +1,23 @@
-// We name pin D0 as led
-int led = D0; 
+// Define the pins we're going to call pinMode on
+int led = D0;  // You'll need to wire an LED to this one to see it blink.
+int led2 = D7; // This one is the built-in tiny one to the right of the USB jack
 
 // This routine runs only once upon reset
-void setup() 
-{
-  // Initialize D0 pin as an output
+void setup() {
+  // Initialize D0 + D7 pin as output
+  // It's important you do this here, inside the setup() function rather than outside it or in the loop function.
   pinMode(led, OUTPUT);
+  pinMode(led2, OUTPUT);
 }
 
-// This routine loops forever 
-void loop() 
-{
-  digitalWrite(led, HIGH);   // Turn ON the LED
+// This routine gets called repeatedly, like once every 5-15 milliseconds.
+// Spark firmware interleaves background CPU activity associated with WiFi + Cloud activity with your code. 
+// Make sure none of your code delays or blocks for too long (like more than 5 seconds), or weird things can happen.
+void loop() {
+  digitalWrite(led, HIGH);   // Turn ON the LED pins
+  digitalWrite(led2, HIGH);
   delay(1000);               // Wait for 1000mS = 1 second
-  digitalWrite(led, LOW);    // Turn OFF the LED
-  delay(1000);               // Wait for 1 second
+  digitalWrite(led, LOW);    // Turn OFF the LED pins
+  digitalWrite(led2, LOW); 
+  delay(1000);               // Wait for 1 second in off mode
 }
