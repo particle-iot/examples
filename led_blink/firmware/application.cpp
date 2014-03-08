d6efe0e773014abb4a4bd2571f31082bbabdd228
Joe Goggins <joe.goggins@gmail.com>
2014-03-08 14:08:59 -0600

    Doc tweak

--- led_blink/firmware/application.cpp
+++ led_blink/firmware/application.cpp
@@ -11,8 +11,7 @@ void setup() {
 }
 
 // This routine gets called repeatedly, like once every 5-15 milliseconds.
-// Spark firmware interleaves background CPU activity associated with WiFi + Cloud activity with your code. 
-// Make sure none of your code delays or blocks for too long (like more than 5 seconds), or weird things can happen.
+// Spark firmware interleaves background CPU activity associated with WiFi + Cloud activity with your code. Note: Code that blocks for too long (like more than 5 seconds), can make weird things happen (like dropping the network connection).  The built-in delay function shown below safely interleaves required background activity, so arbitrarily long delays can safely be done if you need them.
 void loop() {
   digitalWrite(led, HIGH);   // Turn ON the LED pins
   digitalWrite(led2, HIGH);
