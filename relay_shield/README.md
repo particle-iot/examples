afb5d0d9af0937c78b486f0691580a3af21d6946
Joe Goggins <joe.goggins@gmail.com>
2014-03-24 23:22:07 -0500

    relay_shield firmware

---
+++ relay_shield/README.md
@@ -0,0 +1,10 @@
+Relay Shield
+===
+
+This example requires [the relay shield](http://docs.spark.io/#/shields/relay-shield).
+It illustrates how you can use the core to turn on a standard 120-240V circuit like a lamp over the internet.
+
+How to use
+---
+
+    curl TODO
--- relay_shield/firmware/application.cpp
+++ relay_shield/firmware/application.cpp
@@ -1,2 +1,45 @@
-void setup() { }
-void loop() { }
+int RELAY1 = D0;
+int RELAY2 = D1;
+int RELAY3 = D2;
+int RELAY4 = D3;
+
+void setup()
+{
+   //Initilize the relay control pins as output
+   pinMode(RELAY1, OUTPUT);
+   pinMode(RELAY2, OUTPUT);
+   pinMode(RELAY3, OUTPUT);
+   pinMode(RELAY4, OUTPUT);
+   // Initialize all relays to an OFF state
+   digitalWrite(RELAY1, LOW);
+   digitalWrite(RELAY2, LOW);
+   digitalWrite(RELAY3, LOW);
+   digitalWrite(RELAY4, LOW);
+
+   //register the Spark function
+   Spark.function("relay", relayControl);
+}
+
+void loop()
+{
+   // This loops for ever
+}
+
+// command format r1,HIGH
+int relayControl(String command)
+{
+  int relayState = 0;
+  // parse the relay number
+  int relayNumber = command.charAt(1) - '0';
+  // do a sanity check
+  if (relayNumber < 1 || relayNumber > 4) return -1;
+
+  // find out the state of the relay
+  if (command.substring(3,7) == "HIGH") relayState = 1;
+  else if (command.substring(3,6) == "LOW") relayState = 0;
+  else return -1;
+
+  // write to the appropriate relay
+  digitalWrite(relayNumber-1, relayState);
+  return 1;
+}
