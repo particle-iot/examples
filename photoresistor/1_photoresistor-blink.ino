a5f93c4bd04c18d1d220812dcf4db9741da6bad5
Herb Brill <hwbrill@users.noreply.github.com>
2015-05-18 23:45:30 -0500

    Update 1_photoresistor-blink.ino
    I propose the following because: 1. The original version will continuously publish the photoresistor status requiring a Hard Reset to reflash the Device.  2. Didn't include the on Board Led as the example annotation referenced and  3. Added Tinker Digital Write and Analog Read logic to use the Tinker app to test and operate the code...

--- photoresistor/1_photoresistor-blink.ino
+++ photoresistor/1_photoresistor-blink.ino
@@ -11,12 +11,16 @@ It's like a secret message just for you.
 Ready to start?
 ---------------------------------------*/
 
+// Declare Tinker Function used in this project
+    int DO(String command); // Used to set D0, led, on/off with Tinker app
+    int AI(String pin); // Used to read A0, photoresistor, to see analog value with Tinker app
+    
 // We're going to start by declaring which pins everything is plugged into.
-
 int led = D0; // This is where your LED is plugged in. The other side goes to a resistor connected to GND.
 
+    int onBoardLed = D7;  // Added the on board led to the mix
+    
 int photoresistor = A0; // This is where your photoresistor is plugged in. The other side goes to the "power" pin (below).
-
 int power = A5; // This is the other end of your photoresistor. The other side is plugged into the "photoresistor" pin (above).
 // The reason we have plugged one side into an analog pin instead of to "power" is because we want a very steady voltage to be sent to the photoresistor.
 // That way, when we read the value from the other side of the photoresistor, we can accurately calculate a voltage drop.
@@ -26,10 +30,15 @@ int ledOnValue; // This is the average value that the photoresistor reads when t
 int ledOffValue; // This is the average value that the photoresistor reads when the LED is off.
 int ledThreshold; // This is a value halfway between ledOnValue and ledOffValue, above which we will assume the led is on and below which we will assume it is off.
 
+    bool state; // Delcare the state of D7, onBoardLed, as a flag to test in the loop so not to have a continuious publish loop
 
 // We start with the setup function.
 
 void setup() {
+// Tinker Functions used in this project
+Spark.function("digitalwrite", DO); // Changed to make it easier for me to understand how the function works
+Spark.function("analogread", AI);
+
   // The setup function is a part of every microcontroller app.
   // The setup function is called once when your device turns on or resets.
   // Steps taken in this function should be things you only want to have to do once, when your device starts running.
@@ -39,6 +48,7 @@ void setup() {
   pinMode(led,OUTPUT); // Our LED pin is output (lighting up the LED)
   pinMode(photoresistor,INPUT);  // Our photoresistor pin is input (reading the photoresistor)
   pinMode(power,OUTPUT); // The pin powering the photoresistor is output (sending out consistent power)
+pinMode(onBoardLed,OUTPUT); // Add D7, onBoardLed as an OUTPUT
 
   // Next, write the power of the photoresistor to be the maximum possible, which is 4095 in analog.
   analogWrite(power,4095);
@@ -52,6 +62,8 @@ void setup() {
   delay(300); // wait 300 milliseconds
   int off_1 = analogRead(photoresistor); // read photoresistor
   delay(300); // wait 300 milliseconds
+digitalWrite(led,HIGH); // added, missing from example annotation
+delay(300); // added, missing from example annotation
   int on_2 = analogRead(photoresistor); // and do it all again...
   delay(300);
   digitalWrite(led,LOW);
@@ -65,6 +77,9 @@ void setup() {
 
   // Let's also calculate the value between ledOn and ledOff, above which we will assume the led is on and below which we assume the led is off.
   ledThreshold = (ledOnValue+ledOffValue)/2;
+  
+digitalWrite(onBoardLed,HIGH); // turn onBoardLed on going into loop
+state = true; // set status flag before loop
 
 }
 
@@ -73,38 +88,66 @@ void loop() {
   After the setup function is done running, the loop function gets called
   as much as possible and as often as possible, over and over again
   until your device turns off or is reset.
-
   In this loop function, we're going to check to see if the LED is on or off.
   If it is on, we'll send a Spark.publish() to the cloud so that if we want to,
   we can check from other devices when the LED is on or off.
-
   We'll also turn the D7 LED on when the Photoresistor reads that the LED is off.
   
   */
 
   if (analogRead(photoresistor)>ledThreshold) {
     // If you are above the threshold, we'll assume the led is on.
-
+if (state == true) { // onBoardLed is On signifying led is Off...
     // Send a publish...
     Spark.publish("ledStatus","on",60,PRIVATE);
     
     // Wait a second, and then turn the LED off.
-    delay(800);
-    digitalWrite(led,LOW);
+    delay(1000);
+digitalWrite(onBoardLed,LOW); // Changed led to onBoardLed
+state = false; // Added Flag to test so not to have a continuious publish loop!!!
+    }
   }
 
   else {
     // If you are below the threshold, the led is probably off.
-
+if (state == false) { // onBoardLed is On signifying led is On...
     // Send a publish...
     Spark.publish("ledStatus","off",60,PRIVATE);
     
     // Wait a second, and then turn the LED on.
-    delay(800);
-    digitalWrite(led,HIGH);
+    delay(1000);
+digitalWrite(onBoardLed,HIGH); // Changed led to onBoardLed
+state = true; // Added Flag to test so not to have a continuious publish loop!!!
+    }
   }
-  
 }
 
+// End of Loop
 
+int DO(String command){ // Same as 'TinkerDigitalWrite' example
+    bool value = 0;
+    int pinNumber = command.charAt(1) - '0';
+    if (pinNumber< 0 || pinNumber >7) return -1;
+    if(command.substring(3,7) == "HIGH") value = 1;
+    else if(command.substring(3,6) == "LOW") value = 0;
+    else return -2;
+    if(command.startsWith("D")){
+        pinMode(pinNumber, OUTPUT);
+        digitalWrite(pinNumber, value);
+        return 1;}
+    else if(command.startsWith("A")){
+        pinMode(pinNumber+10, OUTPUT);
+        digitalWrite(pinNumber+10, value);
+        return 1;}
+    else return -3;}
 
+int AI(String pin){ // Same as 'TinkerAnalogRead' example
+    int pinNumber = pin.charAt(1) - '0';
+    if (pinNumber< 0 || pinNumber >7) return -1;
+    if(pin.startsWith("D")){
+        pinMode(pinNumber, INPUT);
+        return analogRead(pinNumber);}
+    else if (pin.startsWith("A")){
+        pinMode(pinNumber+10, INPUT);
+        return analogRead(pinNumber+10);}
+    return -2;}
